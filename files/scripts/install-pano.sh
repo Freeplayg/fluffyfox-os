a774ce211b2343a7a32ba75035cc434217be9642
Freeplay <freeplay@duck.com>
2025-05-21 22:09:40 -0400

    Update install-pano.sh

--- files/scripts/install-pano.sh
+++ files/scripts/install-pano.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 wget "https://github.com/oae/gnome-shell-pano/releases/download/v23-alpha5/pano@elhan.io.zip"
 gnome-extensions install pano@elhan.io.zip
+unzip pano@elhan.io.zip -d ~/.local/share/gnome-shell/extensions/pano@elhan.io
 rm pano@elhan.io.zip
