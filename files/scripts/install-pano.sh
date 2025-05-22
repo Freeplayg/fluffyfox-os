6d1cff9c347e9c7a031fe8440ecea89b1fb68dc3
Freeplay <freeplay@duck.com>
2025-05-21 22:06:27 -0400

    Update install-pano.sh

--- files/scripts/install-pano.sh
+++ files/scripts/install-pano.sh
@@ -4,4 +4,3 @@ set -euo pipefail
 wget "https://github.com/oae/gnome-shell-pano/releases/download/v23-alpha5/pano@elhan.io.zip"
 gnome-extensions install pano@elhan.io.zip
 rm pano@elhan.io.zip
-notify-send "Pano - Clipboard Manager installed" "Please relog to enable the extension"
